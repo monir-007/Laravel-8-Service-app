7d1abb3a454791788e73cab76fd619c9a5755b2e
Monir Hossain <monir.hossain@northsouth.edu>
2021-06-08 02:12:27 +0600

    feat(Category Page): insert data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,19 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Auth;
+use Illuminate\Support\Carbon;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-return view('admin.category.index');
+        return view('admin.category.index');
     }
 
-    public function save(Request  $request)
+    public function save(Request $request)
     {
         $validatedData = $request->validate([
-            'name'=>'required|min:3|max:21|unique:categories',
+            'name' => 'required|min:3|max:21|unique:categories',
+        ]);
+
+        Category::insert([
+            'name' => $request->name,
+            'user_id' => Auth::user()->id,
+            'created_at' => Carbon::now()
         ]);
     }
 }
